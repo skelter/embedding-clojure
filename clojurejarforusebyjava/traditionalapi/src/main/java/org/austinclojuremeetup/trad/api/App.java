51e96c39e3b24264b64e4d24561b5ba49297a4a9
Steve Suehs <skelter.net@gmail.com>
2013-06-29 21:50:30 -0500

    mvn archetype:generate a new api project for our demo api

---
+++ clojurejarforusebyjava/traditionalapi/src/main/java/org/austinclojuremeetup/trad/api/App.java
@@ -0,0 +1,13 @@
+package org.austinclojuremeetup.trad.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
