9463c5eaa4f0027e7f4ec9baf34ab1aaa5b417e0
Steve Suehs <skelter.net@gmail.com>
2013-06-30 00:47:14 -0500

    Let's use marginalia for docs

--- clojurejarforusebyjava/wayofpain/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to wayofpain
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- clojurejarforusebyjava/wayofpain/project.clj
+++ clojurejarforusebyjava/wayofpain/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.austinclojuremeetup/traditionalapi "1.0-SNAPSHOT"]]
+  :plugins [[lein-marginalia "0.7.1"]]
   :aot [org.austinclojuremeetup.clojureimpl.Factory])
