bd3d1eb74b2802b80d3d01ea09816afc13f2f97e
Steve Suehs <skelter.net@gmail.com>
2013-06-29 21:59:24 -0500

    Ok..first whack at our hypothetical service

---
+++ clojurejarforusebyjava/traditionalapi/src/main/java/org/austinclojuremeetup/trad/api/TradService.java
@@ -0,0 +1,13 @@
+package org.austinclojuremeetup.trad.api;
+
+import java.util.List;
+
+/**
+ * Some service given to us by the rest of the hypothetical project.
+ */
+interface TradService {
+    /** 
+     * Translate the ordered list of things to Strings for display.
+     */
+    List<String> convert(List<Object> inputs);
+}
