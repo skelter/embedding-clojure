3ccb9b2d70fb5bf9e5700bae2f18219e48070227
Steve Suehs <skelter.net@gmail.com>
2013-07-02 22:20:39 -0500

    App main now dumps all the info on scripting engine

--- scriptingwithclojure/src/main/java/org/austinclojuremeetup/scriptingwithclojure/App.java
+++ scriptingwithclojure/src/main/java/org/austinclojuremeetup/scriptingwithclojure/App.java
@@ -1,5 +1,8 @@
 package org.austinclojuremeetup.scriptingwithclojure;
 
+import java.util.*;
+import javax.script.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	showEngines();
+	if (args.length > 0) {
+	    runScript(args);
+	}
+    }
+
+    private static void showEngines() {
+	// from http://stackoverflow.com/questions/11838369/where-can-i-find-a-list-of-available-jsr-223-scripting-languages
+	ScriptEngineManager mgr = new ScriptEngineManager();
+        List<ScriptEngineFactory> factories = mgr.getEngineFactories();
+
+        for (ScriptEngineFactory factory : factories) {
+
+            System.out.println("ScriptEngineFactory Info");
+
+            String engName = factory.getEngineName();
+            String engVersion = factory.getEngineVersion();
+            String langName = factory.getLanguageName();
+            String langVersion = factory.getLanguageVersion();
+
+            System.out.printf("\tScript Engine: %s (%s)\n", engName, engVersion);
+
+            List<String> engNames = factory.getNames();
+            for(String name : engNames) {
+                System.out.printf("\tEngine Alias: %s\n", name);
+            }
+
+            System.out.printf("\tLanguage: %s (%s)\n", langName, langVersion);
+
+        }
+    }
+
+    private static void runScript(String[] args) {
+	//TODO
     }
 }
