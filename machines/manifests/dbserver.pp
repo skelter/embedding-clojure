215a84eb80772ee55d238905e116cdac25acc104
Steve Suehs <skelter.net@gmail.com>
2013-07-05 09:37:35 -0500

    Open up postgresql so we can connect to it from the host system.

--- machines/manifests/dbserver.pp
+++ machines/manifests/dbserver.pp
@@ -1,3 +1,9 @@
+#include postgresql::server
+
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
+}
+
 group { "puppet":
    ensure => "present",
 }
@@ -5,7 +11,18 @@ group { "puppet":
 File { owner => 0, group => 0, mode => 0644 }
  
 file { '/etc/motd':
-    content => "Welcome to your Vagrant-built virtual machine!
+    content => "Welcome to your Vagrant-built PostgreSQL server
                 Managed by Puppet.\n"
 }
-  
+
+class { 'postgresql::server':
+  config_hash => {
+    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
+    'ip_mask_allow_all_users' => '0.0.0.0/0',
+    'listen_addresses'           => '*',
+    'ipv4acls'                   => ['hostssl all johndoe 192.168.0.0/24 cert'],
+    'manage_redhat_firewall'     => true,
+    'postgres_password'          => 'emb3d',
+   },
+}
+
