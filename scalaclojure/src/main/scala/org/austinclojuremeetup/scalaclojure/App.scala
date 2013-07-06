01d7ce69ff5c95874a0021c4fa2b8ab075e4b90b
Steve Suehs <skelter.net@gmail.com>
2013-07-06 10:12:25 -0500

    comment the example better

--- scalaclojure/src/main/scala/org/austinclojuremeetup/scalaclojure/App.scala
+++ scalaclojure/src/main/scala/org/austinclojuremeetup/scalaclojure/App.scala
@@ -14,8 +14,14 @@ object App {
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
+
+    // now let us play with Clojure
     RT.load("setup")
+
+    // load clojure code from a stream, I found it necessary to specify the name space
     Compiler.load(new StringReader("(ns user) (defn clojurefunc [col] (apply str col))"))
+    
+    // get hold of that func and invoke it
     val cf = RT.`var`("user","clojurefunc")
     val result = cf.invoke(args)
     println( "Result: " + result)
