671e28fe7f99732f5dcca70054d8e173d1724cca
Steve Suehs <skelter.net@gmail.com>
2013-07-06 10:09:41 -0500

    A pretty light example of Scala calling Clojure

---
+++ scalaclojure/src/main/resources/setup.clj
@@ -0,0 +1,1 @@
+(println "setup clj found!")
--- scalaclojure/src/main/scala/org/austinclojuremeetup/scalaclojure/App.scala
+++ scalaclojure/src/main/scala/org/austinclojuremeetup/scalaclojure/App.scala
@@ -1,5 +1,9 @@
 package org.austinclojuremeetup.scalaclojure
 
+import clojure.lang.RT;
+import clojure.lang.Compiler;
+import java.io.StringReader;
+
 /**
  * @author ${user.name}
  */
@@ -10,6 +14,11 @@ object App {
   def main(args : Array[String]) {
     println( "Hello World!" )
     println("concat arguments = " + foo(args))
+    RT.load("setup")
+    Compiler.load(new StringReader("(ns user) (defn clojurefunc [col] (apply str col))"))
+    val cf = RT.`var`("user","clojurefunc")
+    val result = cf.invoke(args)
+    println( "Result: " + result)
   }
 
 }
